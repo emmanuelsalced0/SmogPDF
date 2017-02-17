97916e7700b98c234579ba440dd58086a06682fd
Emmanuel Salcedo <ringer10@hotmail.com>
2017-02-17 12:54:44 -0800

    add User Model and Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217093115) do
+ActiveRecord::Schema.define(version: 20170217204634) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "Name"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 20170217093115) do
     t.float    "Total"
     t.float    "Clientnum"
     t.datetime "created_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
