0bb9f560718b89e11e0b4aec06d911e2067c1d2c
Emmanuel Salcedo <ringer10@hotmail.com>
2017-02-18 10:33:32 -0800

    Sign Up Button fix

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def new
+  @notindex=true
+    @user = User.new
+  end
+
+  def create
+    @notindex=true
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the SmogApp #{@user.username}"
+    redirect_to posts_path
+    else
+    render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
+
