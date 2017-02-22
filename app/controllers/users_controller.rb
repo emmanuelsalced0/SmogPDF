d4d87ad7f4f0fa4e4e79a8e45139f7c69d9f737b
Emmanuel Salcedo <ringer10@hotmail.com>
2017-02-21 17:00:01 -0800

    Fixed login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
    @notindex=true 
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the SmogApp #{@user.username}"
-    redirect_to user_path(@user)
+      redirect_to user_path(@user)
     else
     render 'new'
     end
